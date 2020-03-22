6b2b1ea7aa27a1d8477f1065f4ef1f69e739d048
Denisa Gonciulea <31850720+D3nisa@users.noreply.github.com>
2020-03-22 19:28:17 +0200

    Initial Commit

---
+++ Project1/Project1/Source.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+int interschimbare(int* a, int* b) {
+	int aux;
+	aux = *a;
+	*a = *b;
+	*b = aux;
+}
+int main() {
+	int a, b;
+	printf("Introduceti cele 2 valori:\n");
+	scanf("%d", &a);
+	scanf("%d", &b);
+	interschimbare(&a, &b);
+	printf("Valorile dupa interschimbare sunt: %d %d\n", a, b);
+	system("pause");
+	return 0;
+}
